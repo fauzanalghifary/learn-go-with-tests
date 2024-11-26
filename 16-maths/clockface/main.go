50fc037807ea5167b565dcf896cf0d5b1672fe1c
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-11-26 12:47:39 +0700

    finish chapter 16 - maths

---
+++ 16-maths/clockface/main.go
@@ -0,0 +1,21 @@
+package clockface
+
+import (
+	"os"
+	"time"
+)
+
+// Writes an SVG clockface of the current time to Stdout.
+package main
+
+import (
+"os"
+"time"
+
+"github.com/quii/learn-go-with-tests/math/vFinal/clockface/svg"
+)
+
+func main() {
+	t := time.Now()
+	svg.Write(os.Stdout, t)
+}
